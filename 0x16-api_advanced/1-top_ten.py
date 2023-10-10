edf847c3977da2c52c93c1a90be1877254303746
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-10-11 00:38:18 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """ Top ten """
 
-
 def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts listed for a given subreddit
     """
     from requests import get
-
     url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
 
     headers = {'user-agent': 'my-app/0.0.1'}
@@ -21,17 +19,17 @@ def top_ten(subreddit):
     try:
         js = r.json()
 
-    except ValueError:
-        print(None)
+    except ValueError as e:
+        print("Error parsing JSON response:", e)
         return None
 
     try:
-
         data = js.get("data")
         children = data.get("children")
         for child in children[:10]:
             post = child.get("data")
             print(post.get("title"))
 
-    except:
-        print(None)
+    except Exception as e:
+        print("Error processing data:", e)
+        return None
